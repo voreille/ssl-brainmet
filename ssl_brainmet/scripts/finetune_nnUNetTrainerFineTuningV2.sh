cc4b6fdeeb688a5120a5da41efae7d23e766977c
Valentin Oreiller <valentin.oreiller@gmail.com>
2025-04-23 16:05:47 +0200

    updated for v2 of nnunetrainer

--- ssl_brainmet/scripts/finetune_nnUNetTrainerFineTuningV2.sh
+++ ssl_brainmet/scripts/finetune_nnUNetTrainerFineTuningV2.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# Default GPU ID
+# Default GPU ID and Fold ID
 CUDA_DEVICE=0
 FOLD_ID=0
 
+# Define paths for the raw and preprocessed data
 export nnUNet_raw="/home/valentin/data/target/data/nnUnet_raw"
 export nnUNet_preprocessed="/home/valentin/data/target/data/nnUNet_preprocessed"
-export nnUNet_results="/home/valentin/data/target/data/nnUNet_results_finetuning_v2_exp2"
 
+# Define the path to your YAML configuration file
+CONFIG_FILE="/home/valentin/workspaces/ssl-brainmet/ssl_brainmet/config/finetuning/nnUNetTrainerFineTuningV2_exp3.yaml"
+
+echo "Using configuration file: $CONFIG_FILE"
+
+# Parse the YAML file to extract the results directory.
+nnUNet_results=$(./parse_yaml.py --file $CONFIG_FILE --key  metadata.output_path)
+export nnUNet_results
+
+echo "Storing results in $nnUNet_results"
+
+# I add to add this otherwise nnUNet would not work
 export nnUNet_compile=False
 
+# Task ID
 TASK=510
 
 # Set the CUDA_VISIBLE_DEVICES environment variable
 export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
 
 echo "Running training for fold $FOLD_ID on CUDA device $CUDA_DEVICE..."
-nnUNetv2_train_with_config $TASK 3d_fullres $FOLD_ID -device cuda -num_gpus 1 -tr nnUNetTrainerFineTuningV2 -trainer_config /home/valentin/workspaces/ssl-brainmet/ssl_brainmet/config/finetuning/nnUNetTrainerFineTuningV2.yaml
+nnUNetv2_train_with_config $TASK 3d_fullres $FOLD_ID -device cuda -num_gpus 1 -tr nnUNetTrainerFineTuningV2 -trainer_config $CONFIG_FILE
